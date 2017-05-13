33f9cbf12990173b646fab0e3597ccb13396b52a
Liam Williams <liam.williams@zoho.com>
2017-05-13 16:33:12 +0100

    Renaming

--- app/src/androidTest/java/liam/aercalculator/AERCalculatorApplicationTest.java
+++ app/src/androidTest/java/liam/aercalculator/AERCalculatorApplicationTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
-public class ApplicationTest {
+public class AERCalculatorApplicationTest {
 
     @Test
     public void useAppContext() throws Exception {
